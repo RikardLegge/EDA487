75363007556caf4685482698a94294a2045753ab
Rikard Legge <rikard.legge@gmail.com>
2018-05-10 12:49:16 +0200

    Add gravity

--- shared/game.c
+++ shared/game.c
@@ -8,27 +8,52 @@
 #include "level.h"
 #include "images/won.xbm"
 
-int player_x = 20;
-int player_y = 0;
+typedef struct Player {
+    int x;
+    int y;
+    int dx;
+    int dy;
+} Player;
+
+Player player;
+
+void reset_player() {
+    player.x = 0;
+    player.y = 50;
+}
+
+void player_handle_collission() {
+
+}
+
+void player_step() {
+    player.x += player.dx;
+    player.y += player.dy;
+
+    player_handle_collission();
+
+    player.dy += 1;
+    if(player.dy > 3) {
+        player.dy = 3;
+    }
+
+    player.dx = 0;
+}
 
 void handle_input() {
     if (key_is_active(0x7)) {
-        player_x -= 1;
+        player.dx = -1;
     }
     if (key_is_active(0x9)) {
-        player_x += 1;
+        player.dx = 1;
     }
 
-    if (key_is_active(0x2)) {
-        player_y -= 1;
-    }
-    if (key_is_active(0x0)) {
-        player_y += 1;
+    if (key_is_active(0x5)) {
+        player.dy = -4;
     }
 
     if (key_is_active(0x1)) {
-        player_x = 0;
-        player_y = 0;
+        reset_player();
     }
 }
 
@@ -36,33 +61,33 @@ Sprite won_sprite;
 
 void game_init() {
     canvas_clear();
-    won_sprite = sprite_load(won_bits, won_width, won_height);
     load_levels();
+    reset_player();
+
+    won_sprite = sprite_load(won_bits, won_width, won_height);
 }
 
 void move_player(){
     handle_input();
-    canvas_pixel(player_x,player_y,1);
+    player_step();
+    canvas_pixel(player.x,player.y,0);
 }
 
 void you_lost(){
-    if((player_x < 0) || (player_y < 0)){
+    if((player.x < 0) || (player.y < 0)){
         canvas_clear();
         sprite_draw(&won_sprite, 0, 0);
     }
 }
 
 
-
-
 void game_loop() {
-
     canvas_clear();
     draw_level();
-    canvas_pixel(10,10,0);
+
     move_player();
+
     you_lost();
-    
     canvas_flush();
 }
 
--- shared/level.c
+++ shared/level.c
@@ -7,6 +7,42 @@ void load_levels() {
     levels[0] = sprite_load(level_1_bits, level_1_width, level_1_height);
 }
 
+int level_sample_at(int x, int y) {
+    x /= 4;
+    y /= 4;
+
+    int width = 32;
+    int height = 16;
+    Sprite level = levels[0];
+
+//    return level.data[x * width + height];
+//
+//    int height = sprite->height;
+//    int width = sprite->width;
+//
+//    int sprite_width = width / 8;
+//    if (width % 8 != 0)
+//        sprite_width += 1;
+//
+//    for (int row = 0; row < height; row++) {
+//        for (int col = 0; col < sprite_width; col++) {
+//            uint8_t byte = sprite->data[row * sprite_width + col];
+//            for_range(0, 8) {
+//                for (int sx = 0; sx < scale; sx++) {
+//                    for (int sy = 0; sy < scale; sy++) {
+//                        int set = byte & (0x1 << index);
+//                        if (set && !invert) {
+//                            canvas_pixel((8 * col + index) * scale + sx + x, row * scale + sy + y, 1);
+//                        } else if(!set && invert) {
+//                            canvas_pixel((8 * col + index) * scale + sx + x, row * scale + sy + y, 1);
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+}
+
 void draw_level() {
     sprite_draw_scaled(&levels[0], 0, 0, 4, 1);
 }
